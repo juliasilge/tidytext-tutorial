7caa9e2382b7d0b35baa780a7f6e651d74074945
Julia Silge <julia.silge@gmail.com>
2022-12-23 15:51:04 -0700

    Render to PNG

--- slides/setup.R
+++ slides/setup.R
@@ -1,4 +1,4 @@
-knitr::opts_chunk$set(cache = TRUE, warning = FALSE, message = FALSE)
+knitr::opts_chunk$set(cache = TRUE, warning = FALSE, message = FALSE, dpi = 300)
 library(tidyverse)
 library(silgelib)
 theme_set(theme_plex())
